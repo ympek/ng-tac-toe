0de1b588cca2ae16793d4906c08540acff402e48
ympek <sz@ympek.net>
2017-06-23 01:39:37 +0200

    fixes. it's working though ugly

--- game.js
+++ game.js
@@ -1,115 +1,135 @@
-angular.module('tictactoe', [])
-		.controller('TicTacToeController', GameController);
-	
+angular
+  .module("tictactoe", [])
+  .controller("TicTacToeController", GameController);
 
-	function GameController() {
-		this.state = [];
-		this.currentPlayer = 'X';
-		
-		// inicjalizacja tablicy
-		for (var i = 0; i < 3; i++) {
-			this.state[i] = [];
-			for (var j = 0; j < 3; j++) {
-				this.state[i][j] = '';
-			}
-		}
-	
-	}
-	
-	GameController.prototype.putSign = function ($event) {
-		var id = $event.target.id.split('_');
-		var i = id[0],
-			j = id[1];
-			
-		if (!this.hasPermissionToPutSign(i, j)) {
-			return false;
-		}
-		
-		this.state[i][j] = this.currentPlayer;
-		
-		// sprawdzanie czy ktos wygral
-		this.check();
-		// zmienic gracza
-		this.switchPlayer();
-	}
-	
-	GameController.prototype.hasPermissionToPutSign = function (i, j) {
-		if (this.state[i][j] === '') {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	GameController.prototype.check = function () {
-		// sprawdzanie
-		var xCounter = 0;
-		var oCounter = 0;
+function GameController() {
+  this.state = [];
+  this.currentPlayer = "X";
+  this.movesLeft = 9;
 
-		var winner = false;
-		
-		// poziomy
-		for (var i = 0; i < 3; i++) {
-			for (var j = 0; j < 3; j++) {
-				if (this.state[i][j] == 'O') oCounter++;
-				if (this.state[i][j] == 'X') xCounter++;
-			}
-		}
-		if (oCounter == 3) winner = 'O';
-		if (xCounter == 3) winner = 'X';
+  this.finished = false;
+  this.resultText = "";
+  this.winner = "";
 
-		xCounter = 0; oCounter = 0;
-		// piony
-		for (var i = 0; i < 3; i++) {
-			for (var j = 0; j < 3; j++) {
-				if (this.state[j][i] == 'O') oCounter++;
-				if (this.state[j][i] == 'X') xCounter++;
-			}
-		}
-		if (oCounter == 3) winner = 'O';
-		if (xCounter == 3) winner = 'X';
+  // init array
+  this.initState();
+}
 
-		xCounter = 0; oCounter = 0;
-		// skosy
-		if (this.state[0][0] == 'O') oCounter++;
-		if (this.state[0][0] == 'X') xCounter++;
-		
-		if (this.state[1][1] == 'O') oCounter++;
-		if (this.state[1][1] == 'X') xCounter++;
-		
-		if (this.state[2][2] == 'O') oCounter++;
-		if (this.state[2][2] == 'X') xCounter++;
+GameController.prototype.initState = function() {
+  for (var i = 0; i < 3; i++) {
+    this.state[i] = [];
+    for (var j = 0; j < 3; j++) {
+      this.state[i][j] = "";
+    }
+  }
+};
 
-		if (oCounter == 3) winner = 'O';
-		if (xCounter == 3) winner = 'X';
-		xCounter = 0; oCounter = 0;
-		
-		if (this.state[0][2] == 'O') oCounter++;
-		if (this.state[0][0] == 'X') xCounter++;
-		
-		if (this.state[1][1] == 'O') oCounter++;
-		if (this.state[1][1] == 'X') xCounter++;
-		
-		if (this.state[2][0] == 'O') oCounter++;
-		if (this.state[2][0] == 'X') xCounter++;
+GameController.prototype.putSign = function($event) {
+  var id = $event.target.id.split("_"), 
+  	   i = id[0], 
+	   j = id[1];
 
-		if (oCounter == 3) winner = 'O';
-		if (xCounter == 3) winner = 'X';
-		xCounter = 0; oCounter = 0;
+  if (!this.moveAllowed(i, j)) {
+    return false;
+  }
 
-		if (!winner) {
-			// pat albo jeszcze nie koniec
-		} else {
-			alert("Wygrana gracza" + winner);
-			this.currentPlayer = 'wygrał gracz ' + winner;
-		}
-	}
-	
-	GameController.prototype.switchPlayer = function () {
-		// sprawdzanie
-		if (this.currentPlayer === 'X') {
-			this.currentPlayer = 'O';
-		} else {
-			this.currentPlayer = 'X';
-		}
-	}
+  this.state[i][j] = this.currentPlayer;
+
+  this.movesLeft--;
+
+  this.check();
+  this.switchPlayer();
+};
+
+GameController.prototype.moveAllowed = function(i, j) {
+  if (this.movesLeft === 0 || this.finished) {
+    return false;
+  }
+
+  return this.state[i][j] === "";
+};
+
+GameController.prototype.check = function() {
+  var xCounter = 0;
+  var oCounter = 0;
+
+  var winner = false;
+
+  // poziomy
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (this.state[i][j] == "O") oCounter++;
+      if (this.state[i][j] == "X") xCounter++;
+    }
+    if (oCounter == 3) winner = "O";
+    if (xCounter == 3) winner = "X";
+
+    xCounter = 0;
+    oCounter = 0;
+  }
+
+  // piony
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (this.state[j][i] == "O") oCounter++;
+      if (this.state[j][i] == "X") xCounter++;
+    }
+    if (oCounter == 3) winner = "O";
+    if (xCounter == 3) winner = "X";
+
+    xCounter = 0;
+    oCounter = 0;
+  }
+  // skosy
+  if (this.state[0][0] == "O") oCounter++;
+  if (this.state[0][0] == "X") xCounter++;
+
+  if (this.state[1][1] == "O") oCounter++;
+  if (this.state[1][1] == "X") xCounter++;
+
+  if (this.state[2][2] == "O") oCounter++;
+  if (this.state[2][2] == "X") xCounter++;
+
+  if (oCounter == 3) winner = "O";
+  if (xCounter == 3) winner = "X";
+  xCounter = 0;
+  oCounter = 0;
+
+  if (this.state[0][2] == "O") oCounter++;
+  if (this.state[0][2] == "X") xCounter++;
+
+  if (this.state[1][1] == "O") oCounter++;
+  if (this.state[1][1] == "X") xCounter++;
+
+  if (this.state[2][0] == "O") oCounter++;
+  if (this.state[2][0] == "X") xCounter++;
+
+  if (oCounter == 3) winner = "O";
+  if (xCounter == 3) winner = "X";
+
+  if (!winner) {
+    if (this.movesLeft === 0) {
+      this.endWithTie();
+    }
+  } else {
+    this.end(winner);
+  }
+};
+
+GameController.prototype.switchPlayer = function() {
+  if (this.currentPlayer === "X") {
+    this.currentPlayer = "O";
+  } else {
+    this.currentPlayer = "X";
+  }
+};
+
+GameController.prototype.end = function(winner) {
+  this.resultText = "Wygrał gracz: " + winner;
+  this.finished = true;
+};
+
+GameController.prototype.endWithTie = function(winner) {
+  this.resultText = "Remis!";
+  this.finished = true;
+};
